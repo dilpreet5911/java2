5a3f37dfa62bd7162c7d9b8c8cf2d0bb301b2bfb
dilpreet <dilpreetsingh6255@gmail.com>
2026-04-13 11:41:01 +0530

    Initial commit - Java project setup

---
+++ star_pattren.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class star_pattren {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
