c48c3e6075bbb54a4035bdd4fadc0f124b170dae
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-04-17 21:36:39 +0530

    codes

---
+++ arithemetic.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class arithemetic {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter num");
+        int num = sc.nextInt();
+        System.out.println("enter num2");
+        int num2 = sc.nextInt();
+        System.out.println(num + num2);
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num % num2);
+
+    }
+}
