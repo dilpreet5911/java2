58e6bd60e420afc693529ed5ddf5df3da1369068
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-04-24 21:16:19 +0530

    codes

---
+++ day_3_count.java
@@ -0,0 +1,9 @@
+public class day_3_count {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            count++;
+        }
+        System.out.println(count);
+    }
+}
