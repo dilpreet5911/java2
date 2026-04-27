5a6751bda7b452d9e9f23cda466b8f0355181b0a
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-04-27 21:30:03 +0530

    codes

---
+++ demo.java
@@ -0,0 +1,15 @@
+interface I1 {
+    void method1();
+}
+
+interface I2 {
+    void method2();
+}
+
+interface I3 {
+    void method3();
+}
+
+public class demo implements I1, I2, I3 {
+    public void 
+}
--- reverse.java
+++ reverse.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a number: 
+        ");
         int num = sc.nextInt();
         int reverse = 0;
         int remainder = 0;
