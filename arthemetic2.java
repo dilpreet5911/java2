72e5f92d2dd66b9896c8c73acb8056747e0d1703
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-05-14 20:43:30 +0530

    codes

---
+++ arthemetic2.java
@@ -0,0 +1,14 @@
+public class arthemetic2 {
+    public static void main(String[] arga) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("The sum of a and b is " + sum);
+        System.out.println("The difference of a and b is " + sub);
+        System.out.println("The product of a and b is " + mul);
+        System.out.println("The quotient of a and b is " + div);
+    }
+}
