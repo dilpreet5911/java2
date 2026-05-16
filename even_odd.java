62178631a17370a8c7c75840c397ad67a888630b
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-05-16 13:00:16 +0530

    codes

---
+++ even_odd.java
@@ -0,0 +1,10 @@
+public class even_odd {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+    }
+}
