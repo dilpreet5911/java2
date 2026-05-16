b43604d7b514e80dd8ddba39b45abcb174237399
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-05-16 23:37:55 +0530

    codes

---
+++ swap.java
@@ -0,0 +1,13 @@
+
+public class swap {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        System.out.println("beore swapping a= " + a + " b=" + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("after swapping a= " + a + " and b=" + b);
+    }
+}
