f715be8f6d0e379dda32c96759a41536746ab0b6
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-05-17 22:24:12 +0530

    codes

---
+++ interface_ex.java
@@ -0,0 +1,34 @@
+interface i1 {
+    abstract void show();
+}
+
+interface i2 {
+    abstract void display();
+}
+
+interface i3 {
+    abstract void print();
+}
+
+class myclass implements i1, i2, i3 {
+    public void show() {
+        System.out.println("This is show method");
+    }
+
+    public void display() {
+        System.out.println("This is display method");
+    }
+
+    public void print() {
+        System.out.println("This is print method");
+    }
+}
+
+public class interface_ex {
+    public static void main(String[] args) {
+        myclass obj = new myclass();
+        obj.show();
+        obj.display();
+        obj.print();
+    }
+}
