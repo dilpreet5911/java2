06d3aa50e35953e586ace80726904867166c461f
dilpreet5911 <dilpreetsingh6255@gmail.com>
2026-05-18 22:28:57 +0530

    code

---
+++ constructor.java
@@ -0,0 +1,11 @@
+class Demo {
+    Demo() {
+        System.out.println("Constructor Called");
+    }
+}
+
+public class constructor {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+    }
+}
